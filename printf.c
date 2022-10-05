81b935d3adfd5d978d8872de07a802cc1272c21a
Kai <33795285+kaiicheng@users.noreply.github.com>
2022-10-04 21:26:09 -0400

    Add files via upload

---
+++ printf.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+printf("%s\t%d%\t%f\t%e\n", "hello world", 1, 1.1f , 1.1);
+return 0;
+}
---
+++ puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+char string[] = "This is an example output string\n";
+puts(string);
+char string2[] = "This is an example output string\n";
+puts(string2);
+return 0;
+}
